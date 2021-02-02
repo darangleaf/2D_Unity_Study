5aa25c70d39cd9bddc9788e3368f74b9285616d4
darang <54370981+orderispotato@users.noreply.github.com>
2021-02-03 05:38:54 +0900

    텍스트 읽은 후 출력 기능
    텍스트 읽어서 출력하는 기능

---
+++ text_game_study/Assets/Script/TextScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    public Text ScriptTxt;
+
+    string[] reading = new string[4] {"2021-02-03", "haneul", "haha", " "};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i = 0; reading[i] != " " || i == reading.Length ; i++){
+            ScriptTxt.text += reading[i];
+            ScriptTxt.text += "\n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
